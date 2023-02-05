6f75458cdc2ba6df8bed0f70557dbfb7b85780e0
Patrik Kraif <kraifpatrik@gmail.com>
2023-02-05 10:13:10 +0100

    Better ExpandShaders.py

--- BBMOD_GML/ExpandShaders.py
+++ BBMOD_GML/ExpandShaders.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 import os
 
-CMD_XPANDA = "python3 Xpanda/Xpanda.py"
-PATH_XSHADERS = "Xshaders"
+PATH_SELF = os.path.dirname(os.path.realpath(__file__))
+PATH_XSHADERS = os.path.join(PATH_SELF, "Xshaders")
 PATH_UBER_VS = os.path.join(PATH_XSHADERS, "Uber_VS.xsh")
 PATH_UBER_PS = os.path.join(PATH_XSHADERS, "Uber_PS.xsh")
+CMD_XPANDA = "python3 " + os.path.join(PATH_SELF, "Xpanda", "Xpanda.py")
 
 def dict_to_args_str(_dict: dict) -> str:
     _arg_str = ""
@@ -14,7 +15,7 @@ def dict_to_args_str(_dict: dict) -> str:
     return _arg_str.rstrip()
 
 def expand_shader(_name: str, _defines: dict={}) -> bool:
-    path_out = os.path.join("shaders", _name)
+    path_out = os.path.join(PATH_SELF, "shaders", _name)
     vsh_out = os.path.join(path_out, f"{_name}.vsh")
     fsh_out = os.path.join(path_out, f"{_name}.fsh")
     args = f"--x {PATH_XSHADERS} -ci " + dict_to_args_str(_defines)
@@ -26,37 +27,55 @@ def expand_shader(_name: str, _defines: dict={}) -> bool:
     os.system(command_ps)
     return True
 
-# Default shaders
-expand_shader("BBMOD_ShDefault", { "X_PBR": 1 })
-expand_shader("BBMOD_ShDefaultAnimated", { "X_PBR": 1, "X_ANIMATED": 1 })
-expand_shader("BBMOD_ShDefaultBatched", { "X_PBR": 1, "X_BATCHED": 1 })
-expand_shader("BBMOD_ShDefaultLightmap", { "X_PBR": 1, "X_LIGHTMAP": 1 })
-expand_shader("BBMOD_ShDefaultSprite", { "X_PBR": 1, "X_2D": 1 })
+def combine(*_args: list[dict]) -> dict:
+    res = {}
+    for a in _args:
+        res.update(a)
+    return res
 
-expand_shader("BBMOD_ShDefaultDepth", { "X_OUTPUT_DEPTH": 1 })
-expand_shader("BBMOD_ShDefaultDepthAnimated", { "X_OUTPUT_DEPTH": 1, "X_ANIMATED": 1 })
-expand_shader("BBMOD_ShDefaultDepthBatched", { "X_OUTPUT_DEPTH": 1, "X_BATCHED": 1 })
-expand_shader("BBMOD_ShDefaultDepthLightmap", { "X_OUTPUT_DEPTH": 1, "X_LIGHTMAP": 1 })
+ANIMATED = { "X_ANIMATED": 1 }
+BATCHED = { "X_BATCHED": 1 }
+LIGHTMAP = { "X_LIGHTMAP": 1 }
+SPRITE = { "X_2D": 1 }
+PARTICLES = { "X_PARTICLES": 1 }
+PBR = { "X_PBR": 1 }
+TERRAIN = { "X_TERRAIN": 1 }
+OUTPUT_DEPTH = { "X_OUTPUT_DEPTH": 1 }
+ID = { "X_ID": 1 }
+
+# Default shaders
+expand_shader("BBMOD_ShDefault", PBR)
+expand_shader("BBMOD_ShDefaultAnimated", combine(PBR, ANIMATED))
+expand_shader("BBMOD_ShDefaultBatched", combine(PBR, BATCHED))
+expand_shader("BBMOD_ShDefaultLightmap", combine(PBR, LIGHTMAP))
+expand_shader("BBMOD_ShDefaultSprite", combine(PBR, SPRITE))
+
+expand_shader("BBMOD_ShDefaultDepth", OUTPUT_DEPTH)
+expand_shader("BBMOD_ShDefaultDepthAnimated", combine(OUTPUT_DEPTH, ANIMATED))
+expand_shader("BBMOD_ShDefaultDepthBatched", combine(OUTPUT_DEPTH, BATCHED))
+expand_shader("BBMOD_ShDefaultDepthLightmap", combine(OUTPUT_DEPTH, LIGHTMAP))
 
 expand_shader("BBMOD_ShDefaultUnlit")
-expand_shader("BBMOD_ShDefaultUnlitAnimated", { "X_ANIMATED": 1 })
-expand_shader("BBMOD_ShDefaultUnlitBatched", { "X_BATCHED": 1 })
+expand_shader("BBMOD_ShDefaultUnlitAnimated", ANIMATED)
+expand_shader("BBMOD_ShDefaultUnlitBatched", BATCHED)
 
 # Gizmo shaders
-expand_shader("BBMOD_ShInstanceID", { "X_ID": 1 })
-expand_shader("BBMOD_ShInstanceIDAnimated", { "X_ID": 1, "X_ANIMATED": 1 })
-expand_shader("BBMOD_ShInstanceIDBatched", { "X_ID": 1, "X_BATCHED": 1 })
-expand_shader("BBMOD_ShInstanceIDLightmap", { "X_ID": 1, "X_LIGHTMAP": 1 })
+expand_shader("BBMOD_ShInstanceID", ID)
+expand_shader("BBMOD_ShInstanceIDAnimated", combine(ID, ANIMATED))
+expand_shader("BBMOD_ShInstanceIDBatched", combine(ID, BATCHED))
+expand_shader("BBMOD_ShInstanceIDLightmap", combine(ID, LIGHTMAP))
 
 # Particle shaders
-expand_shader("BBMOD_ShParticleLit", { "X_PARTICLES": 1, "X_PBR": 1 })
-expand_shader("BBMOD_ShParticleUnlit", { "X_PARTICLES": 1 })
-expand_shader("BBMOD_ShParticleDepth", { "X_PARTICLES": 1, "X_OUTPUT_DEPTH": 1 })
+expand_shader("BBMOD_ShParticleUnlit", PARTICLES)
+expand_shader("BBMOD_ShParticleLit", combine(PARTICLES, PBR))
+expand_shader("BBMOD_ShParticleDepth", combine(PARTICLES, OUTPUT_DEPTH))
 
 # Terrain shaders
-expand_shader("BBMOD_ShTerrain", { "X_TERRAIN": 1, "X_PBR": 1 })
-expand_shader("BBMOD_ShTerrainUnlit", { "X_TERRAIN": 1 })
+expand_shader("BBMOD_ShTerrainUnlit", TERRAIN)
+expand_shader("BBMOD_ShTerrain", combine(TERRAIN, PBR))
 
 # Zombie shaders
-expand_shader("ShZombie", { "X_ZOMBIE": 1, "X_ANIMATED": 1, "X_PBR": 1 })
-expand_shader("ShZombieDepth", { "X_ZOMBIE": 1, "X_ANIMATED": 1, "X_OUTPUT_DEPTH": 1 })
+ZOMBIE = combine({ "X_ZOMBIE": 1 }, ANIMATED)
+
+expand_shader("ShZombie", combine(ZOMBIE, PBR))
+expand_shader("ShZombieDepth", combine(ZOMBIE, OUTPUT_DEPTH))
