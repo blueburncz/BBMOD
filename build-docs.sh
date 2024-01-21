e8730378ce075016c5a00d860a1d9172ba6c2737
Patrik Kraif <kraifpatrik@gmail.com>
2024-01-21 15:04:58 +0100

    Fixed path

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,5 @@
 source ../GMDoc/env/bin/activate
 python3 ../GMDoc/gmdoc.py build 
-rm -rf /Applications/XAMPP/htdocs/bbmod/docs/3/*
-cp -r docs_build/* /Applications/XAMPP/htdocs/bbmod/docs/3/
+rm -rf /Applications/XAMPP/xamppfiles/htdocs/bbmod/docs/3/*
+cp -r docs_build/* /Applications/XAMPP/xamppfiles/htdocs/bbmod/docs/3/
 deactivate
